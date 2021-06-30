42dffbd4935691b0ec1981b68030b049b75b5848
Rivka Kremer <rivka.k@develeap.com>
2021-07-01 01:46:57 +0300

    add redis

--- app/counter-service.py
+++ app/counter-service.py
@@ -1,23 +1,27 @@
 #!flask/bin/python
 from flask import Flask, request, request_started
-import configparser
+from redis import Redis, RedisError
 
-config = configparser.ConfigParser()
-config.read("counter-value.ini")
-counter = int(config.get("counter_val", "counter"))
+
+# Connect to Redis
+redis = Redis(host="sentinel.redis.svc.cluster.local", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)
+
+
 @app.route('/', methods=["POST", "GET"])
 def index():
     global counter
     if request.method == "POST":
-        counter+=1
-        config.set('counter_val', 'counter', str(counter))
-
+        try:
+            requests = redis.incr("counter")
+        except RedisError:
+            return "<i>Cannot connect to Redis, counter disabled</i>"
         return "Hmm, Plus 1 to the counter please...\n"
     else:
-        return str(f"My counter is: {counter} ! \nYou are my great champion!\n")
+        requests = redis.get("counter")
+        return str(f"My counter is: {requests} ! \nYou are my great champion!\n")
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=80,host='0.0.0.0')
+    app.run(debug=True, port=80, host='0.0.0.0')
